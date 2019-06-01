5ec5fe69e953bdd2e48051def6ac0fb038b915d1
Douglas Campbell <dacampbelltbone@gmail.com>
2019-06-01 14:51:54 -0400

    styled-components Refactor of TopBar completed.

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -6,7 +6,7 @@ const Card = props => {
       <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.card.img} alt="author image"/>
+          <img src={props.card.img} alt="author of article"/>
         </div>
         <span>By {props.card.author}</span>
       </div>
